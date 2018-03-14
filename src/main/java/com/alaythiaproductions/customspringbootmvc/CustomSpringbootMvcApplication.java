f81a864ea9c09a307b9a331189398c172525c50f
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2018-03-14 16:41:33 -0500

    Initial commit

---
+++ src/main/java/com/alaythiaproductions/customspringbootmvc/CustomSpringbootMvcApplication.java
@@ -0,0 +1,12 @@
+package com.alaythiaproductions.customspringbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSpringbootMvcApplication.class, args);
+	}
+}
